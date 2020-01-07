b246fab08f3875dfe461198adaaecba6916d560a
unknown <jideshvaidya@gmail.com>
2020-01-07 22:21:53 +0545

    added use razor view inside custom tag helpers in asp.net core

---
+++ readDataFromDb/readDataFromDb/Controlers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearnASPNETCoreMVCWithRealApps.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using readDataFromDb.Models;
+
+namespace readDataFromDb.Controlers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            ViewBag.products = db.Product.ToList();
+            return View();
+        }
+        [Route("edit")]
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View("edit");
+        }
+        [Route("edit")]
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        [Route("delete")]
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View("Delete");
+        }
+
+        [Route("delete")]
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            db.Product.Remove(db.Product.Find(id));
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
